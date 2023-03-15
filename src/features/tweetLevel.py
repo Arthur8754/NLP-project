f1286260c5a858a9f88f2efd2aafc86411d9077f
Arthur Baudot <arthur.baudot@telecomnancy.eu>
2023-03-15 13:34:12 -0400

    Test sentiment OpenAI

--- src/features/tweetLevel.py
+++ src/features/tweetLevel.py
@@ -1,4 +1,6 @@
-from features.tokenization import tokenization
+from src.features.tokenization import tokenization
+import os
+import openai
 
 class tweetLevel:
     """ 
@@ -20,3 +22,22 @@ class tweetLevel:
         """
         tokenizer = tokenization()
         return len(tokenizer.tokenize_tweet(tweet))
+    
+    def get_sentiment_tweet(self, tweet):
+        """ 
+        Donne le score de sentiment du tweet. On exploite le playground de OpenAI (https://platform.openai.com/examples/default-tweet-classifier)
+        """
+        # Récupérer l'API KEY : 
+        with open("../OpenAI-settings/API-key.txt") as file:
+            openai.api_key = file.read().split("\n")[0]
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt = f"Give me only the probability the sentiment's tweet to be positive. Tweet:{tweet}",
+            temperature=0,
+            max_tokens=100,
+            top_p=1.0,
+            frequency_penalty = 0.5,
+            presence_penalty=0.0
+        )
+        return response
